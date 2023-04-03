9511694b660598eddcb4b1ecd54bf8deb10bae23
Hex <hex@codeigniter.org.cn>
2023-04-03 22:09:23 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
 import serverConfig from './server/config/index.js';
 
@@ -12,8 +12,8 @@ export default defineConfig({
     alias: {
       '@normal': fileURLToPath(new URL(`./src/${serverConfig.normalUrlPrefix}`, import.meta.url)),
       '@ssr': fileURLToPath(new URL(`./src/${serverConfig.ssrUrlPrefix}`, import.meta.url)),
-      '@static': fileURLToPath(new URL(`./src/static`, import.meta.url)),
+      '@static': fileURLToPath(new URL(`./src/static`, import.meta.url))
     }
   },
   publicDir: false
-})
+});
